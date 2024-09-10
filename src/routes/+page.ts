1fafebdcccb7bc4e837c8d70aa69d431c272181b
husseinamine <contact@husseinamine.com>
2024-09-11 00:22:00 +0300

    missing files

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,5 @@
+export const trailingSlash = 'always';
+
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
