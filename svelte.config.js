7ece56df713a23a7153650c03b2fc24a47082213
husseinamine <contact@husseinamine.com>
2024-09-11 00:42:16 +0300

    i hope this works. deploy issue

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
 		}),
-		// paths: {
-		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		// }
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
